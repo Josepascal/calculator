529197862ae7b781ed0eeaecd874defa7fc41830
Josepascal <Josepascal@users.noreply.github.com>
2023-12-14 12:37:50 -0500

    Add math operator and operate functions

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 let firstNum = 3;
 let secondNum = 7;
-let operator = ["+", "-", "*", "/"];
+let operators = ["+", "-", "*", "/"];
 
-const add = () => {
-    return a + b;
+// Basic Math Operators
+const add = (num1, num2) => {
+    return num1 + num2;
 };
 
-const subtract = () => {
-    return a - b;
+const subtract = (num1, num2) => {
+    return num1 - num2;
 };
 
-const multiply = () => {
-    return a * b;
+const multiply = (num1, num2) => {
+    return num1 * num2;
 };
 
-const divide = () => {
-    return a / b;
+const divide = (num1, num2) => {
+    return num1 / num2;
 };
+
+// Operate Function
+const operate = (operators, num1, num2) => {
+    for (let i = 0; i < operators.length; i++) {
+        if (operators[i] === "+") {
+            add(num1, num2);
+        } else if (operators[i] === "-") {
+            subtract(num1, num2);
+        } else if (operators[i] === "*") {
+            multiply(num1, num2);
+        } else if (operators[i] === "/") {
+            divide(num1, num2);
+        }
+    }
+}
