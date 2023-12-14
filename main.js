2962bf71aa7b0e5df05c76b6833ba0a6fad53c08
Josepascal <Josepascal@users.noreply.github.com>
2023-12-14 12:01:12 -0500

    Add index.html and main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+let firstNum = 3;
+let secondNum = 7;
+let operator = ["+", "-", "*", "/"];
+
+const add = () => {
+    return a + b;
+};
+
+const subtract = () => {
+    return a - b;
+};
+
+const multiply = () => {
+    return a * b;
+};
+
+const divide = () => {
+    return a / b;
+};
